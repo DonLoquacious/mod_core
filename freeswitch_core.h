6db763fd88d86c182650e167bdeed77fee61303e
Donald Maxwell <donald.maxwell@signalwire.com>
2025-05-18 22:59:34 +0000

    Small compilation symbol fix- add some structures to header to get rid of warnings

--- freeswitch_core.h
+++ freeswitch_core.h
@@ -15,11 +15,100 @@ typedef struct mod_core_globals mod_core_globals;
 extern mod_core_globals core_globals;
 #endif
 
+#ifdef _CORE
+#define ATTACH_THREADS
+#endif
+
+#ifdef WIN32
+#define RESULT_FREE(x) CoTaskMemFree(x)
+else
 #define RESULT_FREE(x) free(x)
+#endif
 
 SWITCH_END_EXTERN_C
 
 #ifdef _CORE
+struct switch_core_session {
+	char foo[];
+};
+struct apr_pool_t {
+	char foo[];
+};
+struct switch_channel {
+	char foo[];
+};
+struct apr_thread_t {
+	char foo[];
+};
+struct switch_hash {
+	char foo[];
+};
+struct apr_thread_mutex_t {
+	char foo[];
+};
+struct switch_network_list {
+	char foo[];
+};
+struct switch_xml_binding {
+	char foo[];
+};
+struct apr_sockaddr_t {
+	char foo[];
+};
+struct switch_core_port_allocator {
+	char foo[];
+};
+struct switch_media_bug {
+	char foo[];
+};
+struct switch_rtp {
+	char foo[];
+};
+struct sqlite3_stmt {
+	char foo[];
+};
+struct switch_buffer {
+	char foo[];
+};
+struct switch_ivr_menu {
+	char foo[];
+};
+struct switch_event_node {
+	char foo[];
+};
+struct switch_ivr_digit_stream_parser {
+	char foo[];
+};
+struct sqlite3 {
+	char foo[];
+};
+struct switch_ivr_digit_stream {
+	char foo[];
+};
+struct real_pcre {
+	char foo[];
+};
+struct HashElem {
+	char foo[];
+};
+struct switch_ivr_menu_xml_ctx {
+	char foo[];
+};
+struct apr_file_t {
+	char foo[];
+};
+struct apr_thread_rwlock_t {
+	char foo[];
+};
+struct apr_pollfd_t {
+	char foo[];
+};
+struct apr_queue_t {
+	char foo[];
+};
+struct apr_socket_t {
+	char foo[];
+};
 
 using namespace System;
 using namespace System::Reflection;
